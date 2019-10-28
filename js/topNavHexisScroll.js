ccf30e2482b5326cdd6050327273d491502c09cd
cerebralweb <paulocleon@gmail.com>
2019-10-28 17:58:12 -0500

    new sections & scroll bar

--- js/topNavHexisScroll.js
+++ js/topNavHexisScroll.js
@@ -1,7 +1,7 @@
 window.onscroll = function () { myTopHexisScroll() };
   
 function myTopHexisScroll() {
-  document.body.scrollTop > 400 || document.documentElement.scrollTop > 400
+  document.body.scrollTop > 30 || document.documentElement.scrollTop > 30
     ? document.getElementById('contenedorHeader').style.top = '0'
     : document.getElementById('contenedorHeader').style.top = '-60px';
 }
