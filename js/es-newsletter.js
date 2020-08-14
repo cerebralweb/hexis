f53ffbfb3a4f7d92d9bf199dac847e7646406d88
cerebralweb <paulocleon@gmail.com>
2020-08-14 18:50:24 -0500

    news

--- js/es-newsletter.js
+++ js/es-newsletter.js
@@ -5,12 +5,12 @@ const newsletter_es = `
 <p>Quincenalmente, el equipo editorial de Toc cura una lista con los mejores contenidos sobre
     el consumo online, producidos por Bloomberg, The Economist, YPulse, WARC y Hexis-Toc.
     Suscríbete y recíbela </p>
-<form name="contact" method="POST" netlify netlify-honeypot="bot-field" >            
+    <form name="contact" netlify netlify-honeypot="bot-field">            
     <p><input type="email" name="email" placeholder="email"
-               pattern="[a-zA-Z0-9_.]{5,31}@(([a-zA-Z0-9]{2,6}\.)([a-zA-Z0-9]{2,4})){1}(\.[a-zA-Z0-9]{2,3})?"
+    pattern="[a-zA-Z0-9_.]{5,31}@(([a-zA-Z0-9]{2,6}\.)([a-zA-Z0-9]{2,4})){1}(\.[a-zA-Z0-9]{2,3})?"
                />
     </p>            
-    <p> <button type="submit">Hello Toc</button></p>
+    <p> <button name="button" type="submit">Hello Toc</button></p>
 </form>
 `
 document.getElementById('es-newsletter').insertAdjacentHTML('afterbegin', newsletter_es);
