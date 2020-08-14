4ff7bc9b9dda5bf8b8702e5a81857aef393ae3ba
cerebralweb <paulocleon@gmail.com>
2020-08-14 18:08:19 -0500

    newsletter netlify

--- js/us-newsletter.js
+++ js/us-newsletter.js
@@ -4,7 +4,7 @@ const newsletter_es = `
             
 <p>Every two weeks, the Toc editorial team curates a list of the best content on online consumption, 
 produced by Bloomberg, The Economist, YPulse, WARC and, of course, Hexis-Toc. Subscribe and receive it. </p>
-<form name="contact" method="POST" data-netlify="true">            
+<form name="newsletter" method="POST" data-netlify="true">            
     <p><input type="email" name="email" placeholder="email"
                pattern="[a-zA-Z0-9_.]{5,31}@(([a-zA-Z0-9]{2,6}\.)([a-zA-Z0-9]{2,4})){1}(\.[a-zA-Z0-9]{2,3})?"
                />
