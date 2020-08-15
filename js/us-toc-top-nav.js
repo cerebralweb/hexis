a587ed62e699521f489a277e72d52451adb2581b
cerebralweb <paulocleon@gmail.com>
2020-08-15 13:11:26 -0500

    toc nav

--- js/us-toc-top-nav.js
+++ js/us-toc-top-nav.js
@@ -1,14 +1,19 @@
 
 const top_nav = `  
-<a href="#tip" class="buttonUnit_topNav class1">Toc</a>
+<a href="/es-toc.html" class="buttonUnit_topNav class1">ES</a>
+<a href="https://www.linkedin.com/company/hexis-research-consulting/" 
+   target="_blank"
+   class="buttonUnit_topNav class1"
+   >LinkeIn
+</a>
+<a href="https://twitter.com/HexisToc" target="_blank" class="buttonUnit_topNav class1">Twitter</a>
 <a href="/us-podcast.html" target="_blank" class="buttonUnit_topNav class1">Podcast</a>
 <a href="/us-infographics.html" target="_blank" class="buttonUnit_topNav class1">Infographics</a>
-<a href="https://twitter.com/HexisToc" target="_blank" class="buttonUnit_topNav class1">Twitter</a>
-<a href="https://www.linkedin.com/company/hexis-research-consulting/" target="_blank"
-   class="buttonUnit_topNav class1">LinkeIn</a>
+
 <a href="https://www.instagram.com/hexistoc/" target="_blank" class="buttonUnit_topNav class1">Instagram</a>
+<a href="#tip" class="buttonUnit_topNav class1">Toc</a>
 <a href="/us-home.html" target="_blank" class="buttonUnit_topNav class1">Hexis</a>
-<a href="/es-toc.html" class="buttonUnit_topNav class1">ES</a>
+
 `;
 
 document.getElementById('top_nav').insertAdjacentHTML('afterbegin', top_nav);
