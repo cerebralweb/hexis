8da704c8c487f77159c0cc6cc0d0c5f6573439a1
cerebralweb <paulocleon@gmail.com>
2020-08-19 18:54:32 -0500

    toc 1

--- js/us-fromTheWeb.js
+++ js/us-fromTheWeb.js
@@ -2,14 +2,49 @@
 const infoDeLa_web = ` 
 <h4>Hot Analysis</h4>
 <p>Bloomberg</p>
-<a class="class1" href="/insight/b2b-sala-de-lactacia-y-productividad.html">Someone put this here</a>
+<a 
+class="class1"
+href="https://www.bloomberg.com/news/articles/2020-08-18/the-silicon-valley-tech-exodus-could-be-a-plus?srnd=premium"
+>
+What the ‘Tech Exodus’ Could Mean for Silicon Valley
+</a>
+
 <p>The Economist</p>
-<a class="class1" href="/insight/b2b-sala-de-lactacia-y-productividad.html">Someone put this here</a>
+<a 
+class="class1"
+href="https://www.economist.com/science-and-technology/2020/08/08/a-new-ai-language-model-generates-poetry-and-prose">
+A new AI language model generates poetry and prose
+</a>
+
+
 <p>Ypulse</p>
-<a class="class1" href="/insight/b2b-sala-de-lactacia-y-productividad.html">Someone put this here</a>
+<a 
+class="class1"
+href="https://www.ypulse.com/article/2020/03/02/3-tiktok-trends-brands-should-know/"
+>
+3 TikTok Trends Brands Should Know
+</a>
+
 <p>Hexis TOC</p>
-<a class="class1" href="/insight/b2b-sala-de-lactacia-y-productividad.html">Someone put this here</a>
+<a 
+class="class1" 
+href="https://www.hexis.cc/insight/insight/us-simulating-decision-making-with-consumers.html"
+>
+Health Care: Simulating Consumption Experiences
+</a>
+
 <p>Warc</p>
-<a class="class1" href="/insight/b2b-sala-de-lactacia-y-productividad.html">Productividad
-  de madres millenials ha subido</a>`;
+<a 
+class="class1"
+href="https://www.hexis.cc/pdfs/89258_Netnography_research_-_community_.pdf"
+>
+Netnography: ommunity insights in the cosmetic industry
+</a>
+
+  <p>The Guardian</p>
+  <a 
+  class="class1"
+  href="https://www.theguardian.com/technology/2020/aug/19/facebook-funnelling-readers-towards-covid-misinformation-study"
+  >
+  Facebook funnelling readers towards Covid misinformation - study</a>`;
 document.getElementById('us-fromTheWeb').insertAdjacentHTML('afterbegin', infoDeLa_web);
